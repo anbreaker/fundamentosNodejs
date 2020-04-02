d9725458c5eba0b720e7cdf0b003d4f7d1990b46
anbreaker <antunez19@gmail.com>
2020-04-02 18:38:30 +0200

    trabajando con addons de c++ en nodejs, ej hola mundo

---
+++ modulos/modulosNativosCPlusx2/build/Release/.deps/Release/obj.target/addon/hola.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/addon/hola.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/anbreaker/.cache/node-gyp/12.16.1/include/node -I/home/anbreaker/.cache/node-gyp/12.16.1/src -I/home/anbreaker/.cache/node-gyp/12.16.1/deps/openssl/config -I/home/anbreaker/.cache/node-gyp/12.16.1/deps/openssl/openssl/include -I/home/anbreaker/.cache/node-gyp/12.16.1/deps/uv/include -I/home/anbreaker/.cache/node-gyp/12.16.1/deps/zlib -I/home/anbreaker/.cache/node-gyp/12.16.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hola.o.d.raw   -c -o Release/obj.target/addon/hola.o ../hola.cc
+Release/obj.target/addon/hola.o: ../hola.cc \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/node.h \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8.h \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8-internal.h \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8-version.h \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8config.h \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8-platform.h \
+ /home/anbreaker/.cache/node-gyp/12.16.1/include/node/node_version.h
+../hola.cc:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/node.h:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8.h:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8-internal.h:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8-version.h:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8config.h:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/v8-platform.h:
+/home/anbreaker/.cache/node-gyp/12.16.1/include/node/node_version.h:
---
+++ modulos/modulosNativosCPlusx2/index.js
@@ -0,0 +1,3 @@
+const myAddon = require('./build/Release/addon');
+
+console.log(myAddon.Hola())
