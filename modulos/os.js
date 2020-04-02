3897dca0effd073edfcf20193541811ce04e519d
anbreaker <antunez19@gmail.com>
2020-04-02 20:00:56 +0200

    Modulo os en nodejs, ejemplos de uso

--- modulos/os.js
+++ modulos/os.js
@@ -1,1 +1,25 @@
 //https://nodejs.org/api/os.html
+
+const os = require('os');
+
+console.log(os.arch());
+console.log(os.platform());
+// console.log(os.cpus());
+// console.log(os.constants);
+console.log(os.cpus().length);
+
+const SIZE = 1024;
+function kb(bytes) {return bytes / SIZE}
+function mb(bytes) {return kb(bytes) / SIZE}
+function gb(bytes) {return mb(bytes) / SIZE}
+
+console.log(kb(os.freemem()));
+console.log(mb(os.freemem()));
+console.log(gb(os.freemem()));
+console.log(gb(os.totalmem()));
+
+console.log(os.homedir())
+console.log(os.tmpdir())
+
+console.log(os.hostname())
+console.log(os.networkInterfaces())
