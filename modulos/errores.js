90f719e85e0ca7b7ebff68102ee707e9e6568edd
anbreaker <antunez19@gmail.com>
2020-04-02 13:20:15 +0200

    Trabajando con errores ne nodejs

--- modulos/errores.js
+++ modulos/errores.js
@@ -1,5 +1,35 @@
+function otraFuncion(){
+    seRompe();
+}
+
 function seRompe(){
     return 3 + z
 }
 
-seRompe();
+function seRompeAsincrona(callBack){
+    setTimeout(function (){
+        try{
+            return 3 + z;
+        } catch (error) {
+            console.error('Error en mi funcion asincrona')
+            callBack(error);
+        }
+    })
+}
+
+try{
+    // seRompe();
+    seRompeAsincrona(function(error){
+        console.log(error.message);
+        
+    });
+    otraFuncion();
+} catch(error){
+    console.error('Vaya, algo se ha roto...');
+    console.error(error);
+    console.error(error.message);
+    console.log('Pero lo hemos capturado');
+    
+}
+
+console.log('Esto de aqui esta al final');
